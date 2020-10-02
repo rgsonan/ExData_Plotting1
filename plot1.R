c1f1a22a4bf9f4456b4c17e481aaf8614de2d6cb
Sona Rajagopalan Nagam <60927983+rgsonan@users.noreply.github.com>
2020-10-02 14:13:18 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,37 @@
+## Unzip the dataset
+
+zipfilename <- "exdata_data_household_power_consumption.zip"
+filename <- "household_power_consumption.txt"
+
+if (!file.exists(filename)){
+  unzip(zipfilename) 
+}
+
+## Read the data only for the required dates
+
+data <- read.table(filename,
+           col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+           colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'),
+           skip=66637,nrows=2880,sep =";",na.strings = "?")
+
+## Remove missing values
+data <- data[complete.cases(data),]
+
+## Date formatting
+## data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+##  convert the Date and Time variables to Date/Time classes
+DateTime <- paste(data$Date, data$Time)
+DateTime <- strptime(DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+## Add DateTime column
+data <- cbind(DateTime, data)
+
+par(mfrow=c(1,1))
+
+## Plot1
+hist(data$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency", main="Global Active Power")
+
+## save as png file
+dev.copy(png,"plot1.png",width=480,height=480)
+dev.off()
